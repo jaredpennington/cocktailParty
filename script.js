0fd07fc004dd5c2de809079cb774265e44a3ee63
jcv12 <john.c.vayianos@gmail.com>
2022-03-30 04:17:54 -0400

    fixed the problems. now you can search for drinks and categories

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const searchInput = document.querySelector("[data-search]")
 
 let users = []
 
-searchInput.addEventListener("input", e => {
+searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
+    const isVisable = 
+    user.name.toLowerCase().includes(value) ||
+    user.category.toLowerCase().includes(value) 
+    user.element.classList.toggle("hide", !isVisable)
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://jcv12.github.io/drinksAlcho/")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
@@ -23,8 +23,8 @@ fetch("https://jsonplaceholder.typicode.com/users")
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
       header.textContent = user.name
-      body.textContent = user.email
+      body.textContent = user.category
       userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      return { name: user.name, category: user.category, element: card }
     })
   })
