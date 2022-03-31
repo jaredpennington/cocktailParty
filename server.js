f9da149fcf9e0d4bc9a71436a0996dd898690ac8
acamp <acampbell8610@gmail.com>
2022-03-31 14:50:48 -0500

    added initial handlebars

--- server.js
+++ server.js
@@ -1,22 +1,64 @@
-// Dependencies
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const session = require('express-session');
+const helpers = require('./utils/helper');
 
-const hbs = exphbs.create({});
-
-// Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3021;
+const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/test-routes'));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
+// // Dependencies
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const path = require('path');
+
+// const hbs = exphbs.create({});
+
+// // Sets up the Express App
+// const app = express();
+// const PORT = process.env.PORT || 3021;
+
+// // Set Handlebars as the default template engine.
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(require('./controllers/test-routes'));
+
+// // Starts the server to begin listening
+// app.listen(PORT, () => {
+//   console.log(`Server listening on: http://localhost:${PORT}`);
+// });
