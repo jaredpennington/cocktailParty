8ececdd42c71c945769069cd75b1825390545da5
jcv12 <john.c.vayianos@gmail.com>
2022-04-05 19:09:43 -0400

    pushing to develop

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
