e540079dcfa7cf51c2ced2862323275119719b50
acamp <acampbell8610@gmail.com>
2022-04-07 20:26:21 -0500

    fixed code

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
+    const post_content = document.querySelector('textarea[name="post-content"]').value;
     const photo = document.querySelector('input[name="photo"]').value;
   
     const response = await fetch(`/api/posts`, {
